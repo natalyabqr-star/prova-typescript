672fd4470a4546dba5cbd8c2758c171dc88ed5b7
natalyabqr-star <natalyabqr@gmail.com>
2026-09-22 18:37:59 -0300

    frist commit

---
+++ AulaType/soma.js
@@ -0,0 +1,8 @@
+// Soma js - javascript puro
+const soma = (a, b) => {
+    return a + b;
+}
+console.log(soma(10, 23));
+console.log(soma(10, 30));
+console.log(soma(10, 50));
+console.log(soma('52', 87));
---
+++ AulaType/soma.ts
@@ -0,0 +1,4 @@
+const somaNum = (a: number, b: number): number => {
+    return a + b;
+}
+console.log(somaNum('10', 23));
